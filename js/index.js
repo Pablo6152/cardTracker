6c047fdefb5b560762c25bb3455822a09a5db8ea
Pablo Cisneros <pablocisnerosrojas@gmail.com>
2024-05-10 18:27:16 -0600

    card ids can now be added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ const cardContainer = document.getElementById("card-container")
 const addButton = document.getElementById("add")
 const searchButton = document.getElementById("search")
 
+const modalBackBtn = document.getElementById("modal-back-btn")
+const modalDoneBtn = document.getElementById("modal-done-btn")
+
+const idNumber = document.getElementById("id-number")
+const curp = document.getElementById("curp")
+
+const addModal = document.getElementById("add-modal")
+const modalWindow = document.getElementById("modal-window")
+
+let items = [
+    // {
+    //     idNumber: 123456789,
+    //     name: "Pablo Cisneros",
+    //     curp: "aaaaaa"
+    // }
+]
 
 searchButton.addEventListener("click", () => {
     showWindow(1)
@@ -13,17 +29,50 @@ addButton.addEventListener("click", () => {
 function showWindow(modal){
 
     if(modal === 1){
-        
+        console.log("soon")
     } else if (modal === 2){
-        console.log(modal)
+        addModal.classList.add("show")
+        modalWindow.classList.add("show")
+        addModal.classList.remove("hide")
+        modalWindow.classList.remove("hide")
     }
 
 }
 
-cardContainer.innerHTML= `
+
+modalDoneBtn.addEventListener("click", () => {
+    addItem()
+})
+
+
+function addItem(){
+
+
+    let newItem = {
+        idNumber: idNumber.value,
+        name: "Pablo Cisneros",
+        curp: "aaaaaa"
+    }
+
+    items.push(newItem)
+
+    saveItems()
+    renderItems()
+
+    addModal.classList.add("hide")
+    modalWindow.classList.add("hide")
+    addModal.classList.remove("show")
+    modalWindow.classList.remove("show")
+}
+
+function renderItems(){
+    cardContainer.innerHTML = ""
+
+    for (let i = 0; i < items.length; i++){
+        cardContainer.innerHTML += `
         <div class="card-item">
             <div class="item-first-half">
-                <p class="id-number-preview-text">384-574-987</p>
+                <p class="id-number-preview-text">${items[i].idNumber}</p>
             </div>
             <div class="item-second-half">
                 <span class="material-symbols-outlined item-done-icon item-icon">
@@ -32,10 +81,15 @@ cardContainer.innerHTML= `
                 <span class="material-symbols-outlined item-bookmark-icon item-icon">
                     bookmark
                 </span>
-                <span class="material-symbols-outlined item-close-icon item-icon">
+                <span class="material-symbols-outlined item-erase-icon item-icon">
                     close
                 </span>
             </div>
         </div>
-
     `
+    }
+}
+
+function saveItems(){
+    
+}
